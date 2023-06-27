e0e5c449137c7c75be5470d787bb8b3e1d68606f
Sharon Awuor <sawuor395@gmail.com>
2023-06-27 09:22:07 +0300

    completed

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog
+    # Class body
+    # Instance Method Definition
+    def bark
+      puts "Woof!"
+    end
+  end
+ 
+  class Dog
+    def sit
+        puts "The Dog is sitting"
+    end
+  end
+
+  class Person
+    def talk
+        puts "Hello World!"
+    end
+  end
+
+  class Person
+    def walk
+        puts "The Person is walking"
+    end
+  end
